a6ea31d00e5d5eef02fb538d8fa8006fde5f1752
weo20 <princekumar74011@gmail.com>
2025-03-28 13:12:25 +0530

    update header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
+// import "./customJs.js";
 import CustomJs from "../js/Custom" 
 
 
@@ -29,9 +30,12 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+         >
         {children}
         <CustomJs />
+        <script>
+          
+        </script>
       </body>
     </html>
   );
--- src/js/Custom.js
+++ src/js/Custom.js
@@ -1,10 +1,12 @@
  "use client";
 import { useEffect } from 'react';
+import Script from 'next/script';
 const Custom = () => {
     useEffect(() => {
         const cursor = document.querySelector('.cursor');
         const cursorinner = document.querySelector('.cursor2');
         const links = document.querySelectorAll('a');
+
 
         document.addEventListener('mousemove', function (e) {
             const x = e.clientX;
@@ -46,14 +48,24 @@ const Custom = () => {
                 cursor.classList.remove('hover');
             });
         });
+ 
     }, []);
 
     return (
         <>
+        <Script
+                src="/assets/js/customScript.js"
+                strategy="afterInteractive" // Ensures the script runs after the page is fully loaded
+            />
             <div className="cursor"></div>
-            <div className="cursor2"></div>
+            <div className="cursor2"></div> 
+             
         </>
     );
 };
 
 export default Custom;
+
+
+
+
